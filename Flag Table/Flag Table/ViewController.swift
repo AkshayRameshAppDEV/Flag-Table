d5c43ac9fd2d3d21811be585cea7ff3b84f3c4af
akshayrameshappdev <akshay_ramesh@yahoo.com>
2020-12-06 20:35:15 -0600

    Add initial starter Xcode project

---
+++ Flag Table/Flag Table/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flag Table
+//
+//  Created by Akshay Ramesh on 12/6/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
