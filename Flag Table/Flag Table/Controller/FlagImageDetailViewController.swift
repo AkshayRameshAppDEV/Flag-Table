a1a76ff5403335215294c9a7c5cdd2f79eecbd66
akshayrameshappdev <akshay_ramesh@yahoo.com>
2020-12-12 19:44:20 -0600

    Embedded navigation controller and added black detail view controller for flag image

---
+++ Flag Table/Flag Table/Controller/FlagImageDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FlagImageDetailViewController.swift
+//  Flag Table
+//
+//  Created by Akshay Ramesh on 12/12/20.
+//
+
+import UIKit
+
+class FlagImageDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
